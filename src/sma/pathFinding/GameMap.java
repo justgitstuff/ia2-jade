39ec2a3164947f1b3e010d3df014bd2e18565e1c
david.pc22 <david.pc22@gmail.com>
2011-05-06 14:09:21 +0000

    Pathfindig for the scouts and harversters

---
+++ src/sma/pathFinding/GameMap.java
@@ -0,0 +1,228 @@
+package sma.pathFinding;
+
+import sma.ontology.InfoGame;
+
+
+
+/**
+ * The data map from our example game. This holds the state and context of each tile
+ * on the map. It also implements the interface required by the path finder. It's implementation
+ * of the path finder related methods add specific handling for the types of units
+ * and terrain in the example game.
+ * 
+ */
+public class GameMap {
+	private sma.ontology.InfoGame game;
+	
+	/** The map width in tiles */
+	//public static final int WIDTH = 30;
+	/** The map height in tiles */
+	//public static final int HEIGHT = 30;
+	
+	// MEDIDAS DE NUESTRO SISTEMA VERDADERO
+	public static final int WIDTH=20;
+	public static final int HEIGHT=20;
+	
+	
+	
+	/*
+	 * 1 Building
+	 * 2 Street
+	 * 3 RECYLCING_CENTER
+	 * */
+	
+	public static final int BUILDING = 1;
+	public static final int STREET = 2;
+	public static final int RECYCLING_CENTER = 3;
+	public static final int GARBAGE = 6;
+	
+	public static final int UNDISCOVERED = 10;
+	/*
+	 * Scout = 0
+	 * Harverst = 1
+	 */
+	
+	public static final int SCOUT = 4;
+	public static final int HARVESTER = 5;
+	
+	/** The terrain settings for each tile in the map */
+	private int[][] terrain = new int[WIDTH][HEIGHT];
+	/** The unit in each tile of the map */
+	private int[][] units = new int[WIDTH][HEIGHT];
+	/** Indicator if a given tile has been visited during the search */
+	private boolean[][] visited = new boolean[WIDTH][HEIGHT];
+	
+	/**
+	 * Create a new test map with some default configuration
+	 */
+	public GameMap( InfoGame infoGame) {
+		
+		this.game=infoGame;
+		
+			
+		//this.WIDTH=20;
+		//this.HEIGHT=20;
+		
+		
+		
+		
+		for(int x=0;x<WIDTH;x++){
+			for(int y=0;y<HEIGHT;y++){
+				
+				if (this.game.getCell(x, y)!=null){
+					//System.out.println(this.game.getCell(x, y).getCellType());
+					//System.out.println(this.game.getCell(x, y).isThereAnAgent());
+					//private char garbageType = '-'; //G=Glass, P=Plastic, M=Metal, P=Paper
+					
+					terrain[x][y]=this.game.getCell(x, y).getCellType();
+				
+					
+					// NO SURTEN ELS AGENTS RECONEGUTS
+					if(this.game.getCell(x,y).isThereAnAgent()==true){
+						System.out.println("AGENT TROBATTTT");
+						if(this.game.getCell(x,y).getAgent().getAgentType()==0){
+							units[x][y] = SCOUT;
+							System.out.println("SCOUTTTT");
+						}else{// SERA 1 corresponent HARVESTER
+							units[x][y] = HARVESTER;
+							System.out.println("HARVESTER");
+						}	
+					}
+					
+				}else{
+					terrain[x][y]=UNDISCOVERED;
+				}
+				
+								
+			}
+		}
+		
+	
+	}
+
+	
+	/**
+	 * Clear the array marking which tiles have been visted by the path 
+	 * finder.
+	 */
+	
+	public void clearVisited() {
+		for (int x=0;x<getWidthInTiles();x++) {
+			for (int y=0;y<getHeightInTiles();y++) {
+				visited[x][y] = false;
+			}
+		}
+	}
+	
+	/**
+	 * @see TileBasedMap#visited(int, int)
+	 */
+	public boolean visited(int x, int y) {
+		return visited[x][y];
+	}
+	
+	/**
+	 * Get the terrain at a given location
+	 * 
+	 * @param x The x coordinate of the terrain tile to retrieve
+	 * @param y The y coordinate of the terrain tile to retrieve
+	 * @return The terrain tile at the given location
+	 */
+	public int getTerrain(int x, int y) {
+		return terrain[x][y];
+	}
+	
+	public void setTerrain(int x, int y) {
+		 terrain[x][y]=STREET;
+	}
+	
+	
+	/**
+	 * Get the unit at a given location
+	 * 
+	 * @param x The x coordinate of the tile to check for a unit
+	 * @param y The y coordinate of the tile to check for a unit
+	 * @return The ID of the unit at the given location or 0 if there is no unit 
+	 */
+	public int getUnit(int x, int y) {
+		return units[x][y];
+	}
+	
+	/**
+	 * Set the unit at the given location
+	 * 
+	 * @param x The x coordinate of the location where the unit should be set
+	 * @param y The y coordinate of the location where the unit should be set
+	 * @param unit The ID of the unit to be placed on the map, or 0 to clear the unit at the
+	 * given location
+	 */
+	public void setUnit(int x, int y, int unit) {
+		units[x][y] = unit;
+	}
+	
+	/**
+	 * @see TileBasedMap#blocked(Mover, int, int)
+	 */
+	
+	// Tenir en compte k s0haura de generar dos alternaitvas de bloked...
+	//Incorporar una variable booleana! per calcular les dos possibilitats
+	public boolean blocked(UnitMover mover, int x, int y, int option) {
+		// if theres a unit at the location, then it's blocked
+		if (getUnit(x,y) != 0) {
+			return true;
+		}
+		
+		int unit = ((UnitMover) mover).getType();
+		
+		
+		switch(option){
+		// OPTION 
+		// Nomes passa per les caselles STREET
+			case 1:
+				if (unit == SCOUT || unit == HARVESTER ) {
+					return ((terrain[x][y] != STREET));
+				}
+				break;
+			
+		// OPTION 2
+		// Nomes passa per les caselles STREET y UNDISCOVERED 
+			case 2:
+				if (unit == SCOUT || unit == HARVESTER ) {
+					return ((terrain[x][y] != STREET)&&(terrain[x][y] != UNDISCOVERED));
+				}
+				break;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * @see TileBasedMap#getCost(Mover, int, int, int, int)
+	 */
+	public float getCost(UnitMover mover, int sx, int sy, int tx, int ty) {
+		return 1;
+	}
+
+	/**
+	 * @see TileBasedMap#getHeightInTiles()
+	 */
+	public int getHeightInTiles() {
+		return WIDTH;
+	}
+
+	/**
+	 * @see TileBasedMap#getWidthInTiles()
+	 */
+	public int getWidthInTiles() {
+		return HEIGHT;
+	}
+
+	/**
+	 * @see TileBasedMap#pathFinderVisited(int, int)
+	 */
+	public void pathFinderVisited(int x, int y) {
+		visited[x][y] = true;
+	}
+	
+	
+}
---
+++ src/sma/pathFinding/PathTest.java
@@ -0,0 +1,202 @@
+package sma.pathFinding;
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import sma.ontology.InfoGame;
+
+
+
+/**
+ * A simple test to show some path finding at unit
+ * movement for a tutorial at http://www.cokeandcode.com
+ * 
+ */
+public class PathTest{
+	/** The map on which the units will move */
+	private GameMap map;
+	/** The path finder we'll use to search our map */
+	private AStarPathFinder finder;
+	/** The last path found for the current unit */
+	private Path path;
+	
+	/** The list of tile images to render the map */
+	private Image[] tiles = new Image[6];
+	/** The offscreen buffer used for rendering in the wonder world of Java 2D */
+	private Image buffer;
+	
+	/** The x coordinate of selected unit or -1 if none is selected */
+	private int selectedx = -1;
+	/** The y coordinate of selected unit or -1 if none is selected */
+	private int selectedy = -1;
+	
+	/** The x coordinate of the target of the last path we searched for - used to cache and prevent constantly re-searching */
+	private int lastFindX = -1;
+	/** The y coordinate of the target of the last path we searched for - used to cache and prevent constantly re-searching */
+	private int lastFindY = -1;
+	
+	/**
+	 * Create a new test game for the path finding tutorial
+	 */
+	public PathTest(InfoGame infoGame) {
+		
+		
+	 	map= new GameMap(infoGame);
+		
+		
+		finder = new AStarPathFinder(map, 500);
+		
+		/*	
+		PosicioInicial(15,15,1);
+		Path stepsPathFinal= PosicioFinal(25,8,1);
+		
+		//distancia(stepsPathFinal);
+		//stepsFinals(stepsPathFinal);
+		int distPesosOp1= distanciaPesos(stepsPathFinal);
+		
+				
+		// OPCIOOOOO 2
+		PosicioInicial(15,15,2);
+		Path stepsPathFinal2= PosicioFinal(25,8,2);
+		
+		int distPesosOp2 = distanciaPesos(stepsPathFinal2);
+		
+		// Distancia pitjor dels casos 
+		int distFinal= distPesosOp1;
+		
+		if(distFinal>distPesosOp2)distFinal=distPesosOp2;
+		
+		System.out.println("Distancia FINAL" + distFinal);
+		*/
+		
+	}
+	
+	
+  
+	 
+	 /**
+		 * Handle the mouse being pressed. If the mouse is over a unit select it. Otherwise we move
+		 * the selected unit to the new target (assuming there was a path found)
+		 * 
+		 * @param x The x coordinate of the mouse cursor on the screen
+		 * @param y The y coordinate of the mouse cursor on the screen
+		 */
+		public void PosicioInicial(int x, int y, int option) {
+		
+			
+			if ((x < 0) || (y < 0) || (x >= map.getWidthInTiles()) || (y >= map.getHeightInTiles())) {
+				return;
+			}
+			
+			if (map.getUnit(x, y) != 0) {
+				selectedx = x;
+				selectedy = y;
+				lastFindX = - 1;
+			} else {
+				//if (true){
+					map.clearVisited();
+					path = finder.findPath(new UnitMover(map.getUnit(selectedx, selectedy)), 
+							   			   selectedx, selectedy, x, y, option);
+					
+					if (path != null) {
+						path = null;
+						int unit = map.getUnit(selectedx, selectedy);
+						map.setUnit(selectedx, selectedy, 0);
+						map.setUnit(x,y,unit);
+						selectedx = x;
+						selectedy = y;
+						lastFindX = - 1;
+					}
+			//	}
+			}
+		}
+
+	/**
+	 * Handle the mouse being moved. In this case we want to find a path from the
+	 * selected unit to the position the mouse is at
+	 * 
+	 * @param x The x coordinate of the mouse cursor on the screen
+	 * @param y The y coordinate of the mouse cursor on the screen
+	 */
+
+// TENIR EN COMPTE K SERA UN STREET				
+	public Path PosicioFinal(int x, int y, int option) {		
+		//if (true) {
+			// D'acord l'algoritme la posicio final interve en el calcul! per a k sigui possible la convertime temporalment en una 
+			// posicio accesible(STREET)
+			map.setTerrain(x, y);
+			
+			if ((lastFindX != x) || (lastFindY != y)) {
+				lastFindX = x;
+				lastFindY = y;
+				path = finder.findPath(new UnitMover(map.getUnit(selectedx, selectedy)), selectedx, selectedy, x, y, option);		
+			}
+		//}
+		
+		return path;
+	}
+		
+	/* Distancia sense tenir en compte pesos caselles
+	public int distancia(Path path){
+		System.out.println(path.getLength());
+			return path.getLength();
+	}*/
+	
+
+	public int distanciaPesos(Path path){
+		int x,y,dist=0;
+		
+		for(int i=0;i<path.getLength();i++){
+			
+			//System.out.println(" Pas"+ (i+1) + ":  " + path.getStep(i).getX()+" "+ path.getStep(i).getY());
+			 x=path.getStep(i).getX();
+			 y=path.getStep(i).getY();
+			
+			 //WATER
+			 if( map.getTerrain(x, y) == GameMap.UNDISCOVERED)dist=dist+2; else dist++;
+			 
+			// System.out.println(map.getTerrain(x, y));
+		}			
+		System.out.println(dist);
+			return dist;
+	}
+	
+	public void stepsFinals(Path path){
+		
+		System.out.println("----Passsos a seguir-----");
+		// Imprimeix els passos k realitza!
+		for(int i=0;i<path.getLength();i++){
+			System.out.println(" Pas"+ (i+1) + ":  " + path.getStep(i).getX()+" "+ path.getStep(i).getY());
+		}								
+		System.out.println("-----------------------");
+}
+
+	
+	
+	
+	/**
+	 * Entry point to our simple test game
+	 * 
+	 * @param argv The arguments passed into the game
+	 */
+	//public static void main(String[] argv) {
+		
+	//	PathTest test = new PathTest();		
+		
+		
+	//}
+}
